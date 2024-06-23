eb3b41a97bbe3dfe87e17b1f1e4fbcc54ff88ba0
David7309 <davidbps37@gmail.com>
2024-06-23 17:56:00 -0500

    Se agrega serializers, vistas y configuraciones para Django REST Framework

--- taskmanager/taskmanager/tasks/models.py
+++ taskmanager/taskmanager/tasks/models.py
@@ -1,11 +1,8 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     description = models.TextField()
     completed = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
--- taskmanager/taskmanager/tasks/tests.py
+++ taskmanager/taskmanager/tasks/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from tasks.models import Task
 
-# Create your tests here.
+class TaskAPITestCase(TestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        
+    def test_create_task(self):
+        self.task1 = Task.objects.create(title="Tarea 1", description="Descripción de la tarea 1", completed=False, user=self.user)
+        # Asegúrate de realizar las afirmaciones necesarias para verificar que la tarea se haya creado correctamente
+        
+    def test_delete_task(self):
+        self.task1 = Task.objects.create(title="Tarea 1", description="Descripción de la tarea 1", completed=False, user=self.user)
+        # Asegúrate de realizar las afirmaciones necesarias para verificar que la tarea se pueda eliminar correctamente
+        
+    def test_list_tasks(self):
+        self.task1 = Task.objects.create(title="Tarea 1", description="Descripción de la tarea 1", completed=False, user=self.user)
+        # Asegúrate de realizar las afirmaciones necesarias para verificar que las tareas se puedan listar correctamente
+        
+    def test_update_task(self):
+        self.task1 = Task.objects.create(title="Tarea 1", description="Descripción de la tarea 1", completed=False, user=self.user)
+        # Asegúrate de realizar las afirmaciones necesarias para verificar que la tarea se pueda actualizar correctamente
