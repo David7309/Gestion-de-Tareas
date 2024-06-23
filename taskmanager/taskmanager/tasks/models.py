c9d1ccec09254c339669f13612830ce86eba5f63
David7309 <davidbps37@gmail.com>
2024-06-23 17:07:39 -0500

    Definir modelos User y Task, crear y aplicar migraciones

--- taskmanager/taskmanager/tasks/models.py
+++ taskmanager/taskmanager/tasks/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
